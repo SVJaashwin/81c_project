23684a1038c8e799fc4b81dc1ea3fa314e71e159
SVJaashwin <88593780+SVJaashwin@users.noreply.github.com>
2021-11-11 17:18:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var canvas = document.getElementById("OLYMPIC_LOGO");
+ctx=canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = "5";
+ctx.arc(200,200,50,0,2*Math.PI);
+
+ctx.strokeStyle = "orange";
+ctx.lineWidth = "5";
+ctx.arc(250,250,0,2*Math.PI);
+
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = "5";
+ctx.arc(200,200,50,0,2*Math.PI);
+
+
+ctx.strokeStyle = "green";
+ctx.lineWidth = "5";
+ctx.arc(200,200,50,0,2*Math.PI);
+
+
+ctx.strokeStyle = "red";
+ctx.lineWidth = "5";
+ctx.arc(200,200,50,0,2*Math.PI);
+ctx.stroke();
